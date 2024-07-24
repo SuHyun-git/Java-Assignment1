4523df01ba5782b226864ba834ec40220f1d69cd
SuHyun <46856071+SuHyun-git@users.noreply.github.com>
2024-07-25 08:28:04 +0900

    level 2-2

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,4 +2,29 @@ package calculator;
 
 public class Calculator {
 
+    public int calculate(int num1, int num2, String sign) {
+        int result = 0;
+
+        if (sign.equals("+")) {
+            result = num1 + num2;
+        } else if (sign.equals("-")) {
+            result = num1 - num2;
+        } else if (sign.equals("*")) {
+            result = num1 * num2;
+        } else if (sign.equals("/")) {
+            try {
+                result = num1 / num2;
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                return -1;
+            }
+        } else {
+            System.out.println("정확한 기호를 입력해주세요.");
+            return -1;
+        }
+
+        return result;
+    }
+
 }
